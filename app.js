738b207eadd9396075474836cc42a44db7d90f02
sme4web <sme.dev212@gmail.com>
2024-12-02 22:46:21 +0200

    change in port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const port = 3300;
+const port = 3300 || process.env.PORT;
 
 app.use(express.json());
 app.use(cors())
